ecd53b3d376d3d9ba34a1d0561aea0eff7e08871
Eidoline <sofia.ilinykh@arcadia.spb.ru>
2022-02-11 19:19:15 +0300

    added Task_2

--- AutomationCourse_HomeWork/Task_1.cs
+++ AutomationCourse_HomeWork/Task_1.cs
@@ -8,11 +8,11 @@ namespace AutomationCourse_HomeWork
         {
             Console.WriteLine("Please add three arguments for the futher operations:");
             Console.Write("x = ");
-            int x = Convert.ToInt32(Console.ReadLine());
+            double x = Convert.ToInt32(Console.ReadLine());
             Console.Write("y = ");
-            int y = Convert.ToInt32(Console.ReadLine());
+            double y = Convert.ToInt32(Console.ReadLine());
             Console.Write("z = ");
-            int z = Convert.ToInt32(Console.ReadLine());
+            double z = Convert.ToInt32(Console.ReadLine());
 
             double result1 = (x + y) * (1 + Math.Sqrt(z));
             Console.WriteLine($"The result of the first statement: {result1}");          
--- Task_2/Task_2.cs
+++ Task_2/Task_2.cs
@@ -2,11 +2,20 @@
 
 namespace Task_2
 {
-    class Program
+    class Task_2
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please add three dimensions of the box for the futher operations:");
+            Console.Write("Width: ");
+            double width = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Height: ");
+            double height = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Length: ");
+            double length = Convert.ToInt32(Console.ReadLine());
+
+            double diagonal = Math.Sqrt(Math.Pow(length, 2) + Math.Pow(width, 2) + Math.Pow(height, 2));
+            Console.WriteLine($"The length of the diagonal: {diagonal}");
         }
     }
 }
