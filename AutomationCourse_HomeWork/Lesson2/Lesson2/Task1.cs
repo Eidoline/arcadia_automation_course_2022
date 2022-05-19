33bc605b08d7fb7de09956e2c98b9897515e55b9
Eidoline <sofia.ilinykh@arcadia.spb.ru>
2022-05-19 23:02:51 +0300

    resolved the second lesson tasks

---
+++ AutomationCourse_HomeWork/Lesson2/Lesson2/Task1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Lesson2
+{
+    class Task1
+    {
+        static void Main()
+        {
+            const string login = "Sofya";
+            const string passwordHash = "938D34FFA2846893EDC65227A8EC5B0C9F8CC39E31D5B61CD65DF697248B925B"; // password = "somepassword123"
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("For sign in add your credentials.");
+                Console.Write("Login: ");
+                string userLogin = Console.ReadLine();
+                Console.Write("Password: ");
+
+                string userPassword = Console.ReadLine();
+                SHA256 sha256Hash = SHA256.Create();
+                byte[] userPasswordBytes = Encoding.UTF8.GetBytes(userPassword);
+                byte[] userPasswordHashBytes = sha256Hash.ComputeHash(userPasswordBytes);
+                string userHash = BitConverter.ToString(userPasswordHashBytes).Replace("-", String.Empty);
+
+                if (userLogin == login && userHash == passwordHash)
+                {
+                    Console.WriteLine("You are let in the system.");
+                    return;
+                }
+            }
+            Console.WriteLine("Error: credentials are not correct. Access denied.");
+        }
+    }
+}
---
+++ AutomationCourse_HomeWork/Lesson2/Task2/Task2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Task2
+{
+    class Task2
+    {
+        static Func<double, double, double> Addition = (x, y) => (x + y);
+        static Func<double, double, double> Subtraction = (x, y) => (x - y);
+        static Func<double, double, double> Division = (x, y) => (x / y);
+        static Func<double, double, double> Multiplication = (x, y) => (x * y);
+        static Func<double, double, double> Exponentiation = (x, y) => Math.Pow(x, y);
+
+        public static double ReadOperand()
+        {
+            double parsedNumber;
+            while (true)
+            {
+                string userOperand = Console.ReadLine();
+                if (double.TryParse(userOperand, out parsedNumber)) break;
+                Console.WriteLine("Not valid value, try again:");
+            }
+            return parsedNumber;
+        }
+       static public Func<double, double, double> ReadOperator()
+        {
+            string userOperator;
+            while(true)
+            {
+                userOperator = Console.ReadLine();
+                switch (userOperator)
+                {
+                    case "+":
+                        return Addition;
+                    case "-":
+                        return Subtraction;
+                    case "/":
+                        return Division;
+                    case "*":
+                        return Multiplication;
+                    case "^":
+                        return Exponentiation;
+                    default:
+                        Console.WriteLine("Not valid value, try again:");
+                        break;
+                }
+
+            }
+        }
+        static void Main()
+        {
+            Console.Write("Enter the first operand: ");
+            double operand1 = ReadOperand();
+            Console.Write("Enter the second operand: ");
+            double operand2 = ReadOperand();
+            Console.Write("Enter the operator: ");
+            Func<double, double, double> action = ReadOperator();
+            double result = action(operand1, operand2);
+            Console.WriteLine($"The result for your operation: {result}");
+        }
+    }
+}
